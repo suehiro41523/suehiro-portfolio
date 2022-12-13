0fc7e853f3fc91aaee882514848d5261035360d5
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2022-12-13 16:44:29 +0900

    update install eslint and prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as component from './component';
-import ReactMarkdown from 'react-markdown';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import * as component from './component'
+import ReactMarkdown from 'react-markdown'
 
-function App() {
-  const [estate, setEstate] = useState<any>([]);
+function App(): any {
+  const [estate, setEstate] = useState<any>([])
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await component.readEstates();
-      setEstate(result.data.data);
-    };
-    fetchData();
-  }, []);
-  console.log(estate);
+    const fetchData = async (): Promise<any> => {
+      const result = await component.readEstates()
+      setEstate(result.data.data)
+    }
+    void fetchData()
+  }, [])
+  console.log(estate)
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <ul>
           {estate.map((x: any, index: number) => (
             <li key={index}>
-              <img
-                src={component.url + x.attributes.image.data[0].attributes.url}
-                alt=''
-              />
+              <img src={component.url + x.attributes.image.data[0].attributes.url} alt="" />
               <ReactMarkdown>{x.attributes.Description}</ReactMarkdown>
             </li>
           ))}
         </ul>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
